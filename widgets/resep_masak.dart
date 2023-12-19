6c6303e5a9237f5afb8800f251b9501c5a449350
littleeinstein21 <154114113+littleeinstein21@users.noreply.github.com>
2023-12-19 07:28:52 +0700

    Update resep_masak.dart

--- widgets/resep_masak.dart
+++ widgets/resep_masak.dart
@@ -1,25 +1,14 @@
 import 'package:bisa/dummy_data.dart';
-import 'package:bisa/firestore_dataUser.dart';
-import 'package:bisa/widgets/video_masak.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final db = FirebaseFirestore.instance;
 
-class ResepMasakanScreen extends StatefulWidget {
+class ResepMasakanScreen extends StatelessWidget {
   final int index;
 
   const ResepMasakanScreen({Key? key, required this.index}) : super(key: key);
-
-  @override
-  _ResepMasakanScreenState createState() => _ResepMasakanScreenState();
-}
-
-class _ResepMasakanScreenState extends State<ResepMasakanScreen> {
-  List<String> selectedItems = [];
-  Map<String, dynamic>? data;
-  List<dynamic>? resep;
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +27,7 @@ class _ResepMasakanScreenState extends State<ResepMasakanScreen> {
       ),
       backgroundColor: Colors.white,
       body: FutureBuilder<DocumentSnapshot>(
-        future: db.collection('dataMasak').doc(dataMasak[widget.index].nama).get(),
+        future: db.collection('dataMasak').doc(dataMasak[index].nama).get(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
@@ -54,12 +43,9 @@ class _ResepMasakanScreenState extends State<ResepMasakanScreen> {
           } else if (!snapshot.hasData || !snapshot.data!.exists) {
             return Text('Document does not exist');
           } else {
-            // Document exists in the database
-            data = snapshot.data!.data() as Map<String, dynamic>;
+            final data = snapshot.data!.data() as Map<String, dynamic>;
 
-            // Extract data
-            resep = List<String>.from(data!['resep'] ?? []);
-            String videoUrl = data!['videoUrl'] ?? ''; // Tambahkan ini untuk mendapatkan URL video
+            final resep = data['resep'] as List<dynamic>?;
 
             return Column(
               children: [
@@ -77,31 +63,23 @@ class _ResepMasakanScreenState extends State<ResepMasakanScreen> {
                                   height: 24,
                                   decoration: BoxDecoration(
                                     shape: BoxShape.circle,
-                                    color: Colors.blue, // Change circle color as needed
+                                    color: Colors.blue,
                                   ),
                                   child: Center(
                                     child: Text(
                                       '${index + 1}',
                                       style: TextStyle(
-                                        color: Colors.white, // Change text color to white
+                                        color: Colors.white,
                                       ),
                                     ),
                                   ),
                                 ),
-                                SizedBox(width: 8), // Add some spacing between number and text
+                                SizedBox(width: 8),
                                 Expanded(
                                   child: Text(resep?[index]),
                                 ),
                               ],
                             ),
-                            onTap: () {
-                              _playVideo(videoUrl); // Memanggil fungsi untuk memutar video
-                              setState(() {
-                                // Use a Set for selectedItems
-                                selectedItems.add(resep?[index]);
-                                Firestore_Datasource().AddCart(selectedItems.toList());
-                              });
-                            },
                           ),
                           const Divider(
                             color: Colors.black,
@@ -116,18 +94,6 @@ class _ResepMasakanScreenState extends State<ResepMasakanScreen> {
             );
           }
         },
-      ),
-    );
-  }
-
-  void _playVideo(String videoUrl) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => VideoMemasakScreen(
-          videoUrl: videoUrl,
-          nama: dataMasak[widget.index].nama,
-        ),
       ),
     );
   }
